f44972a782cb591a30810c62a75c2eb5eed2d3fd
hbaltiDDD <126068548+hbaltiDDD@users.noreply.github.com>
2023-03-02 10:42:53 +0100

    add breakfast menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,10 @@
 import streamlit
 streamlit.title('My parents New Healthy Dinner')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Bluberry')
+streamlit.text('Kale & spinach')
+streamlit.text('hard-boiled free range egg')
+
+
+
+
