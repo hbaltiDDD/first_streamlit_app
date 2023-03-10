5180fcec31e5ab7bd1896a7093484b1edbbb8757
hbaltiDDD <126068548+hbaltiDDD@users.noreply.github.com>
2023-03-10 07:54:05 +0100

    import snowflake.connector

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit
 import pandas
 import requests
-import snwoflake.connector
+import snowflake.connector
 
 streamlit.title('My parents New Healthy Dinner')
 streamlit.header('Breakfast Menu')
